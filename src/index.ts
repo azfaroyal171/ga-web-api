c36e6121eb7de12a1355ea544710bee8a3b4fad0
Azfar Haziq <azfarhaziq171@gmail.com>
2024-10-16 15:20:40 +0900

    create trigger endpoint

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
-import express, {Express} from 'express';
+import express, {Express, Request, Response} from 'express';
 import dotenv from 'dotenv';
+import {trigger} from './helpers/githubAction';
 
 const app: Express = express();
 const port = 1026;
@@ -9,6 +10,28 @@ app.use(express.json());
 
 app.get('/', (_, res) => {
   res.send('Hello World');
+});
+
+app.post('/trigger', async (req: Request, res: Response) => {
+  const body = req.body;
+  const workflowId: string = body.workflowId.toString();
+  const inputs: Record<string, string> = JSON.parse(body.inputs);
+
+  if (!workflowId || !inputs || !process.env.GITHUB_TOKEN) {
+    res.status(400).send('Invalid request');
+    return;
+  }
+
+  try {
+    await trigger({
+      workflowId,
+      inputs,
+      token: process.env.GITHUB_TOKEN,
+    });
+    res.send('Github Action triggered');
+  } catch (error) {
+    res.status(500).send('Error triggering Github Action');
+  }
 });
 
 app.listen(port, () => {
